617d25ff6fec70312667f252b97ac92666aa3893
Chris Maurer <chris.maurer@wnco.com>
2013-07-30 22:19:33 -0500

    added multi bar chart

--- src/directives/nvd3Directives.js
+++ src/directives/nvd3Directives.js
@@ -122,4 +122,71 @@ angular.module('nvd3ChartDirectives', [])
                 })
             }
         }
-    });
+    })
+    .directive('nvd3MultiBarChart', function(){
+        return {
+            restrict: 'E',
+            scope: {
+                data: '=',
+                width: '@',
+                height: '@',
+                id: '@',
+                showlegend: '@',
+                tooltips: '@',
+                showcontrols: '@',
+                reducexticks: '@',
+                staggerlabels: '@',
+                rotatelabels: '@',
+                transitionduration: '@',
+                margin: '&'
+            },
+            link: function(scope, element, attrs){
+                scope.$watch('data', function(data){
+                    if(data){
+                        nv.addGraph({
+                            generate: function(){
+                                var margin = {left:50, top:50, bottom:50, right:50};
+                                var width = attrs.width - (margin.left + margin.right),
+                                    height = attrs.height - (margin.top + margin.bottom);
+
+                                var chart = nv.models.multiBarChart()
+                                    .margin(margin)
+                                    .x(function(d){ return d[0] })
+                                    .y(function(d){ return d[1] })
+                                    .width(width)
+                                    .height(height)
+                                    .showLegend(attrs.showlegend === undefined ? false : (attrs.showlegend === "true"))
+                                    .showControls(attrs.showcontrols === undefined ? false : (attrs.showcontrols === "true"))
+                                    .tooltips(attrs.tooltips === undefined ? false : (attrs.tooltips  === "true"))
+                                    .reduceXTicks(attrs.reducexticks === undefined ? false: (attrs.reducexticks === "true"))
+                                    .staggerLabels(attrs.staggerlabels === undefined ? 0 : attrs.staggerlabels)
+                                    .rotateLabels(attrs.rotatelabels === undefined ? 0 : attrs.rotatelabels);
+
+                                chart.xAxis
+                                    .tickFormat(function(d) {
+                                        return d3.time.format('%X')(new Date((d*1000)))
+                                    });
+
+                                chart.yAxis
+                                    .tickFormat(d3.format(',2f'));
+
+                                d3.select('#' + attrs.id + ' svg')
+                                    .attr('height', height)
+                                    .attr('width', width)
+                                    .datum(data)
+                                    .transition().duration((attrs.transitionduration === undefined ? 500 : attrs.transitionduration))
+                                    .call(chart);
+
+                                nv.utils.windowResize(chart.update);
+
+                                return chart;
+                            },
+                            callback: function(graph){
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    })
+;
