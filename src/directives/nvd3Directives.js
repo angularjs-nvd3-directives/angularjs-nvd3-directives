fe51bc49153f3fa44f231e25ea23e8a2b60143be
Chris Maurer <chris.maurer@wnco.com>
2013-07-30 22:00:27 -0500

    Added stacked area chart, and example

--- src/directives/nvd3Directives.js
+++ src/directives/nvd3Directives.js
@@ -64,4 +64,62 @@ angular.module('nvd3ChartDirectives', [])
                 })
             }
         }
+    })
+    .directive('nvd3StackedAreaChart', function(){
+        return {
+            restrict: 'E',
+            scope: {
+                data: '=',
+                width: '@',
+                height: '@',
+                id: '@',
+                showlegend: '@',
+                tooltips: '@',
+                showcontrols: '@',
+                margin: '&'
+            },
+            link: function(scope, element, attrs){
+                scope.$watch('data', function(data){
+                    if(data){
+                        nv.addGraph({
+                            generate: function(){
+                                var margin = {left:50, top:50, bottom:50, right:50};
+                                var width = attrs.width - (margin.left + margin.right),
+                                    height = attrs.height - (margin.top + margin.bottom);
+
+                                var chart = nv.models.stackedAreaChart()
+                                    .margin(margin)
+                                    .x(function(d){ return d[0] })
+                                    .y(function(d){ return d[1] })
+                                    .width(width)
+                                    .height(height)
+                                    .showLegend(attrs.showlegend === undefined ? false : (attrs.showlegend === "true"))
+                                    .showControls(attrs.showcontrols === undefined ? false : (attrs.showcontrols === "true"))
+                                    .tooltips(attrs.tooltips === undefined ? false : (attrs.tooltips  === "true"));
+
+                                chart.xAxis
+                                    .tickFormat(function(d) {
+                                        return d3.time.format('%X')(new Date((d*1000)))
+                                    });
+
+                                chart.yAxis
+                                    .tickFormat(d3.format(',2f'));
+
+                                d3.select('#' + attrs.id + ' svg')
+                                    .attr('height', height)
+                                    .attr('width', width)
+                                    .datum(data)
+                                    .call(chart);
+
+                                nv.utils.windowResize(chart.update);
+
+                                return chart;
+                            },
+                            callback: function(graph){
+                            }
+                        })
+                    }
+                })
+            }
+        }
     });
