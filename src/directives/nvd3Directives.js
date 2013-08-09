3e1fb09ed6f8329b76172630238858cf1cb8a581
Chris Maurer <chris.maurer@wnco.com>
2013-08-08 22:48:39 -0500

    started adding support for d3.js, nvd3.js configuration

--- src/directives/nvd3Directives.js
+++ src/directives/nvd3Directives.js
@@ -15,9 +15,50 @@ angular.module('nvd3ChartDirectives', [])
                 showyaxis: '@',
                 rightalignyaxis: '@',
                 defaultstate: '@',
-                nodata: '@',
+                nodata: '&',
                 margin: '&',
-                tooltipcontent: '&'
+                tooltipcontent: '&',
+                color: '&',
+                //xaxis
+                xaxisorient: '&',
+                xaxisticks: '&',
+                xaxistickvalues: '&',
+                xaxisticksubdivide: '&',
+                xaxisticksize: '&',
+                xaxistickpadding: '&',
+                xaxistickformat: '&',
+                xaxislabel: '&',
+                xaxisscale: '&',
+                xaxisdomain: '&',
+                xaxisrange: '&',
+                xaxisrangeband: '&',
+                xaxisrangebands: '&',
+                xaxisshowmaxmin: '@',
+                xaxishighlightzero: '@',
+                xaxisrotatelables: '@',
+                xaxisrotateylabel: '@',
+                xaxisstaggerlabels: '@',
+
+                //yaxis
+                yaxisorient: '&',
+                yaxisticks: '&',
+                yaxistickvalues: '&',
+                yaxisticksubdivide: '&',
+                yaxisticksize: '&',
+                yaxistickpadding: '&',
+                yaxistickformat: '&',
+                yaxislabel: '&',
+                yaxisscale: '&',
+                yaxisdomain: '&',
+                yaxisrange: '&',
+                yaxisrangeband: '&',
+                yaxisrangebands: '&',
+                yaxisshowmaxmin: '@',
+                yaxishighlightzero: '@',
+                yaxisrotatelables: '@',
+                yaxisrotateylabel: '@',
+                yaxisstaggerlabels: '@'
+
             },
             link: function(scope, element, attrs){
                 scope.$watch('data', function(data){
@@ -38,19 +79,121 @@ angular.module('nvd3ChartDirectives', [])
                                     .showXAxis(attrs.showxaxis === undefined ? false : (attrs.showxaxis  === "true"))
                                     .showYAxis(attrs.showyaxis === undefined ? false : (attrs.showyaxis  === "true"))
                                     .rightAlignYAxis(attrs.rightalignyaxis === undefined ? false : (attrs.rightalignyaxis  === "true"))
-                                    .noData(attrs.nodata === undefined ? 'No Data Available.' : attrs.nodata);
+                                    .noData(attrs.nodata === undefined ? 'No Data Available.' : scope.nodata());
 
                                 if(attrs.tooltipcontent){
-                                    chart.tooltipContent(scope.tooltipcontent);
+                                    chart.tooltipContent(scope.tooltipcontent());
+                                }
+                                if(attrs.color){
+                                    chart.color(scope.color());
+                                }
+                                if(attrs.xaxisorient){
+                                    chart.xAxis.orient(scope.xaxisorient());
+                                }
+                                if(attrs.xaxisticks){
+                                    chart.xAxis.ticks(scope.xaxisticks());
+                                }
+                                if(attrs.xaxistickvalues){
+                                    chart.xAxis.tickValues(scope.xaxistickvalues());
+                                }
+                                if(attrs.xaxisticksubdivide){
+                                    chart.xAxis.tickSubdivide(scope.xaxisticksubdivide());
+                                }
+                                if(attrs.xaxisticksize){
+                                    chart.xAxis.tickSize(scope.xaxisticksize());
+                                }
+                                if(attrs.xaxistickpadding){
+                                    chart.xAxis.tickPadding(scope.xaxistickpadding());
+                                }
+                                if(attrs.xaxistickformat){
+                                    chart.xAxis.tickFormat(scope.xaxistickformat());
+                                }
+                                if(attrs.xaxislabel){
+                                    chart.xAxis.axisLabel(scope.xaxislabel());
+                                }
+                                if(attrs.xaxisscale){
+                                    chart.xAxis.xScale(scope.xaxisscale());
+                                }
+                                if(attrs.xaxisdomain){
+                                    chart.xAxis.domain(scope.xaxisdomain());
+                                }
+                                if(attrs.xaxisrange){
+                                    chart.xAxis.range(scope.xaxisrange());
+                                }
+                                if(attrs.xaxisrangeband){
+                                    chart.xAxis.rangeBand(scope.xaxisrangeband());
+                                }
+                                if(attrs.xaxisrangebands){
+                                    chart.xAxis.rangeBands(scope.xaxisrangebands());
+                                }
+                                if(attrs.xaxisshowmaxmin){
+                                    chart.xAxis.showMaxMin((scope.xaxisshowmaxmin === "true"));
+                                }
+                                if(attrs.xaxishighlightzero){
+                                    chart.xAxis.highlightZero((scope.xaxishighlightzero === "true"));
+                                }
+                                if(attrs.xaxisrotatelables){
+                                    chart.xAxis.highlightZero(scope.xaxisrotatelables);
+                                }
+                                if(attrs.xaxisrotateylabel){
+                                    chart.xAxis.rotateYLabel((scope.xaxisrotateylabel === "true"));
+                                }
+                                if(attrs.xaxisstaggerlabels){
+                                    chart.xAxis.staggerlabels((scope.xaxisstaggerlabels === "true"));
                                 }
 
-                                chart.xAxis
-                                    .tickFormat(function(d) {
-                                        return d3.time.format('%x')(new Date(d))
-                                });
-
-                                chart.yAxis
-                                    .tickFormat(d3.format(',2f'));
+                                //yaxis
+                                if(attrs.yaxisticks){
+                                    chart.yAxis.ticks(scope.yaxisticks());
+                                }
+                                if(attrs.yaxistickvalues){
+                                    chart.yAxis.tickValues(scope.yaxistickvalues());
+                                }
+                                if(attrs.yaxisticksubdivide){
+                                    chart.yAxis.tickSubdivide(scope.yaxisticksubdivide());
+                                }
+                                if(attrs.yaxisticksize){
+                                    chart.yAxis.tickSize(scope.yaxisticksize());
+                                }
+                                if(attrs.yaxistickpadding){
+                                    chart.yAxis.tickPadding(scope.yaxistickpadding());
+                                }
+                                if(attrs.yaxistickformat){
+                                    chart.yAxis.tickFormat(scope.yaxistickformat());
+                                }
+                                if(attrs.yaxislabel){
+                                    chart.yAxis.axisLabel(scope.yaxislabel());
+                                }
+                                if(attrs.yaxisscale){
+                                    chart.yAxis.xScale(scope.yaxisscale());
+                                }
+                                if(attrs.yaxisdomain){
+                                    chart.yAxis.domain(scope.yaxisdomain());
+                                }
+                                if(attrs.yaxisrange){
+                                    chart.yAxis.range(scope.yaxisrange());
+                                }
+                                if(attrs.yaxisrangeband){
+                                    chart.yAxis.rangeBand(scope.yaxisrangeband());
+                                }
+                                if(attrs.yaxisrangebands){
+                                    chart.yAxis.rangeBands(scope.yaxisrangebands());
+                                }
+                                if(attrs.yaxisshowmaxmin){
+                                    chart.yAxis.showMaxMin((scope.yaxisshowmaxmin === "true"));
+                                }
+                                if(attrs.yaxishighlightzero){
+                                    chart.yAxis.highlightZero((scope.yaxishighlightzero === "true"));
+                                }
+                                if(attrs.yaxisrotatelables){
+                                    chart.yAxis.highlightZero(scope.yaxisrotatelables);
+                                }
+                                if(attrs.yaxisrotateylabel){
+                                    chart.yAxis.rotateYLabel((scope.yaxisrotateylabel === "true"));
+                                }
+                                if(attrs.yaxisstaggerlabels){
+                                    chart.yAxis.staggerlabels((scope.yaxisstaggerlabels === "true"));
+                                }
 
                                 d3.select('#' + attrs.id + ' svg')
                                     .attr('height', height)
