4e8fa354e616d20e3561433bdf07891ed3be8e46
Chris Maurer <chris.maurer@wnco.com>
2014-02-13 09:02:12 -0600

    removed axis directive

--- dist/angularjs-nvd3-directives.js
+++ dist/angularjs-nvd3-directives.js
@@ -1,4 +1,4 @@
-/*! angularjs-nvd3-directives - v0.0.5-beta - 2014-01-27
+/*! angularjs-nvd3-directives - v0.0.5-beta - 2014-02-13
  * http://cmaurer.github.io/angularjs-nvd3-directives
  * Copyright (c) 2014 Christian Maurer; Licensed Apache License, v2.0 */
 ( function () {
