04afcdee0993115c2d7a7854c338ec54d4e68aca
Chris Maurer <chris.maurer@wnco.com>
2014-04-02 08:20:35 -0500

    updated grunt-contrib-jshint to 0.10.0

--- dist/angularjs-nvd3-directives.js
+++ dist/angularjs-nvd3-directives.js
@@ -1,4 +1,4 @@
-/*! angularjs-nvd3-directives - v0.0.5-beta - 2014-04-01
+/*! angularjs-nvd3-directives - v0.0.5-beta - 2014-04-02
  * http://cmaurer.github.io/angularjs-nvd3-directives
  * Copyright (c) 2014 Christian Maurer; Licensed Apache License, v2.0 */
 ( function () {
