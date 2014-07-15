bbb9faac8be1291eafb3cada8987b0b4a34222fc
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-15 18:59:58 +0200

    Even shorter events configuration.

--- src/directives/nvD3Events.js
+++ src/directives/nvD3Events.js
@@ -1,6 +1,10 @@
 function processEvents(chart, scope) {
 
   function maybeAddListener(object, args) {
+    if (!object) {
+      return;
+    }
+
     angular.forEach(args, function (name) {
       if (object[name]) {
         object.dispatch.on(name + '.directive', function () {
@@ -12,41 +16,21 @@ function processEvents(chart, scope) {
     });
   }
 
-  if (chart.dispatch) {
-    maybeAddListener(chart, [ 'tooltipShow', 'tooltipHide', 'beforeUpdate', 'stateChange', 'changeState' ]);
-  }
-
-  if (chart.lines) {
-    maybeAddListener(chart.lines, [ 'elementMouseover.tooltip', 'elementMouseout.tooltip', 'elementClick' ]);
-  }
-
-  if (chart.stacked && chart.stacked.dispatch) {
-    maybeAddListener(chart.stacked, [ 'areaClick.toggle', 'tooltipShow', 'tooltipHide' ]);
-  }
+  maybeAddListener(chart.dispatch && chart, [ 'tooltipShow', 'tooltipHide', 'beforeUpdate', 'stateChange', 'changeState' ]);
+  maybeAddListener(chart.lines, [ 'elementMouseover.tooltip', 'elementMouseout.tooltip', 'elementClick' ]);
+  maybeAddListener(chart.stacked && chart.stacked.dispatch && chart.stacked, [ 'areaClick.toggle', 'tooltipShow', 'tooltipHide' ]);
 
   if (chart.interactiveLayer) {
-    if (chart.interactiveLayer.elementMouseout) {
-      maybeAddListener(chart.interactiveLayer, [ 'elementMouseout' ]);
-    }
-
-    if (chart.interactiveLayer.elementMousemove) {
-      maybeAddListener(chart.interactiveLayer, [ 'elementMousemove' ]);
-    }
+    maybeAddListener(chart.interactiveLayer.elementMouseout && chart.interactiveLayer, [ 'elementMouseout' ]);
+    maybeAddListener(chart.interactiveLayer.elementMousemove && chart.interactiveLayer, [ 'elementMousemove' ]);
   }
 
   // this adds elementClick for scatter and bullet - not sure if it is ok
   angular.forEach(['discretebar', 'multibar', 'pie', 'scatter', 'bullet' ],
       function (element) {
-        if (chart[element]) {
-          maybeAddListener(chart[element], [ 'elementMouseover.tooltip', 'elementMouseout.tooltip', 'elementClick' ]);
-        }
+        maybeAddListener(chart[element], [ 'elementMouseover.tooltip', 'elementMouseout.tooltip', 'elementClick' ]);
       });
 
-  if (chart.legend) {
-    maybeAddListener(chart.legend, [ 'stateChange.legend', 'legendClick', 'legendDblclick', 'legendMouseover' ]);
-  }
-
-  if (chart.controls && chart.controls.legendClick) {
-    maybeAddListener(chart.controls, [ 'legendClick' ]);
-  }
+  maybeAddListener(chart.legend, [ 'stateChange.legend', 'legendClick', 'legendDblclick', 'legendMouseover' ]);
+  maybeAddListener(chart.controls && chart.controls.legendClick && chart.controls, [ 'legendClick' ]);
 }
