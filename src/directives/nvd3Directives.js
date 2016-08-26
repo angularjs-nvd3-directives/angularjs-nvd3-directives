8ab9d2062107c26bd6ab1a9e27e162f03d745dbd
Vishal Sharma <vishal2033@gmail.com>
2016-08-26 17:29:47 +0530

    [#1887] x15 fork og ng-nvd3-directive: added tooltip content value formatter for pie charts

--- src/directives/nvd3Directives.js
+++ src/directives/nvd3Directives.js
@@ -1210,7 +1210,8 @@
                     objectequality: '@',
 
                     //d3.js specific
-                    transitionduration: '@'
+                    transitionduration: '@',
+                    tooltippiecontentvalueformatter: '&'
 
                 },
                 controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs){
@@ -1226,7 +1227,10 @@
 
                         if(data){
                             //if the chart exists on the scope, do not call addGraph again, update data and call the chart.
-                            if(scope.chart){
+                            if(scope.chart) {
+                                if (Array.isArray(data) && data.length > 0 && attrs.tooltipcontent && attrs.tooltippiecontentvalueformatter) {
+                                  scope.tooltippiecontentvalueformatter()(scope.chart.tooltip, data);
+                                }
                                 return scope.d3Call(data, scope.chart);
                             }
                             nv.addGraph({
